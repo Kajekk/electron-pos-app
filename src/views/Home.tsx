f4cef63af2d09d975657e0e06b93dbc68af9b111
Thong <thong.nt@teko.vn>
2020-11-06 16:46:33 +0700

    :sparkles: Add home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,25 +1,33 @@
 import { Grid } from "@material-ui/core";
-import { Link } from "react-router-dom";
-import FeatureCard from "../components/FeatureCard";
 import React from "react";
+import { Link } from "react-router-dom";
 import OrderIcon from "../assets/svg/order.svg";
-import StatsIcon from "../assets/svg/stats.svg";
 import SettingIcon from "../assets/svg/settings.svg";
+import StatsIcon from "../assets/svg/stats.svg";
+import FeatureCard from "../components/FeatureCard";
 const Home: React.FC<any> = ({ classes }) => {
   return (
     <Grid container spacing={8}>
       <Grid item xs={12} sm={12} md={4} lg={3} xl={3}>
         <Link to={"/products"}>
           <FeatureCard
-            title={
-              <React.Fragment>
-                Sản phẩm
-              </React.Fragment>
-            }
+            title={<React.Fragment>Bán hàng</React.Fragment>}
+            SvgSource={OrderIcon}
+            content={"Thực hiện thao tác bán hàng"}
+            style={{
+              backgroundImage: "linear-gradient(to right, #3aade7, #1876d2)",
+            }}
+          />
+        </Link>
+      </Grid>
+      <Grid item xs={12} sm={12} md={4} lg={3} xl={3}>
+        <Link to={"/products"}>
+          <FeatureCard
+            title={<React.Fragment>Sản phẩm</React.Fragment>}
             SvgSource={OrderIcon}
             content={"Quản lí danh sách sản phẩm"}
             style={{
-              backgroundImage: "linear-gradient(to right, #3aade7, #1876d2)",
+              backgroundImage: "linear-gradient(to right, #a2379c, #4B1248)",
             }}
           />
         </Link>
--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,14 +1,32 @@
-import { Paper } from '@material-ui/core';
+import { AppBar, Button, Paper, Toolbar, Typography } from "@material-ui/core";
 import "./Layout.scss";
-import * as React from 'react';
-export interface ILayoutProps {
-}
+import * as React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+export interface ILayoutProps {}
 
 export const Layout: React.FunctionComponent<ILayoutProps> = ({ children }) => {
-
-    return (
-        <div className="layout">
-            {children}
-        </div>
-    );
-}
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            // className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            //   className={classes.title}
+          >
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className="layout">{children}</div>
+    </>
+  );
+};
